59640675b1c69d22990559f9c68edc5681c9b935
Asan <isliamov1@gmail.com>
2022-05-12 20:53:09 +0300

    Fix

--- src/components/Forms/Forms.jsx
+++ src/components/Forms/Forms.jsx
@@ -3,33 +3,16 @@ import style from './Forms.module.css';
 
 function Forms() {
   return (
-    <div className={style.wrapper}>
-      <Form>
-        <Row className="mb-3">
-          <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-          </Form.Group>
-          <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-          </Form.Group>
-          <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-          </Form.Group>
-          <Form.Group as={Col} controlId="formGridState">
-            <Form.Label>State</Form.Label>
-            <Form.Select defaultValue="Choose...">
-              <option>Choose...</option>
-              <option>...</option>
-            </Form.Select>
-          </Form.Group>
-        </Row>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+    <div className={style.container}>
+      <h2>Contact</h2>
+      <form>
+        <div className={style.wrapper}>
+          <div className="box">
+            <input type="text" name="fname" id="" />
+            <label for="fname">First </label>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
